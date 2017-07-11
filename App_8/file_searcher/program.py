28f7ae57ec03b1ea924a7fd31f6915b7b7dd1e36
David Kananda <davidkananda@gmail.com>
2017-07-11 06:31:33 +0100

    main project structure

---
+++ App_8/file_searcher/program.py
@@ -0,0 +1,14 @@
+
+
+def main():
+    print_header()
+
+
+def print_header():
+    print('--------------------------------------------')
+    print('        FILE SEARCHER APP')
+    print('--------------------------------------------')
+
+
+if __name__ == '__main__':
+    main()
